2b3ab72dbaf1e2df60b12e6cdcd7a76d8d0985e2
ryuryu88@rquare.co.kr <ryuryu88@rquare.co.kr>
2020-09-09 17:46:52 +0900

    useState를 이용하여 카운터 만들기

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Hello from './Hello';
 import InputSample from './InputSample';
 import Wrapper from './Wrapper';
-
+import Counter from './Counter';
 
 function App() {
  
-  return (
-    <Wrapper name = "너는 누구냐">
-      <Hello name="유희짱" isSpecial= {true}/>
-      <Hello name="나는 거짓말이지롱"/>
-    </Wrapper>
+  return ( 
+    <Counter/>
   );
 }
-//안녕
+
 export default App;
